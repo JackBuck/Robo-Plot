ad693e13c5aeb1f2acae136deb66f51ec16798b2
Max Redmond <Max.Redmond@Renishaw.com>
2016-11-09 21:53:08 +0000

    #5 :: Flawed attempt interleaving different numbers of steps on each motor.
    NB - This method will move both motors at equal speeds until the first one reaches its limit. It will then continue to
    move the second motor until it also reaches the specified limit.
    
    [Jack] I think I found the bug which meant only one motor was turning in the lab - haven't tested it though!

--- src/StepperControl.py
+++ src/StepperControl.py
@@ -7,54 +7,68 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 
 # Defining GPIO signals
+# Pins 11,15,16,18
 # GPIO17,GPIO22,GPIO23,GPIO24
-StepPins = [17, 22, 23, 24]
-StepPins2 = [4, 14, 15, 27]
+step_pins = [17, 22, 23, 24]
+step_pins_2 = [4, 14, 15, 27]
 
 # Set all pins as output
-for pin in StepPins:
+for pin in step_pins:
     GPIO.setup(pin, GPIO.OUT)
     GPIO.output(pin, False)  # keeps it off
-for pin in StepPins2:
+for pin in step_pins_2:
     GPIO.setup(pin, GPIO.OUT)
     GPIO.output(pin, False)
 
 # sequence
-Seq = [[1, 0, 1, 0], [0, 1, 1, 0], [0, 1, 0, 1], [1, 0, 0, 1]] # For large stepper motors
-# Seq2 = [[0, 1, 1, 0], [0, 0, 1, 1], [1, 0, 0, 1], [1, 1, 0, 0]] # For small stepper motors
+seq = [[1, 0, 1, 0], [0, 1, 1, 0], [0, 1, 0, 1], [1, 0, 0, 1]]  # For large stepper motors
+# seq_2 = [[0, 1, 1, 0], [0, 0, 1, 1], [1, 0, 0, 1], [1, 1, 0, 0]] # For small stepper motors
 
-StepCount = len(Seq)  # max length of sequence
-StepDir = 1  # Set to 1 or 2 for clockwise; Set to -1 or -2 for anti-clockwise
-
-WaitTime = 1 / 1000
+# max length of sequence
+step_dir = 1  # Set to 1 or 2 for clockwise; Set to -1 or -2 for anti-clockwise
 
 # Initialise variables
-StepCounter = 0  # so it will start at the first item in the list
+wait_time = 10 / 1000  # default wait time change it in code so its easier.
+num_rotations_1 = int(sys.argv[1])  # number of loops around the sequence wanted for motor 1 and 2
+num_rotations_2 = int(sys.argv[2])
 
-# Start main loop
-for number in range(int(sys.argv[1])):  # how many times you want to loop
+step_counter_1 = 0  # so it will start at the first item in the list. Holds the postion of which list its using in seq or seq_2
+step_counter_2 = 0
+loop_control = True
+rotation_holder_1 = 0  # to represent how many loops of the sequence have been completed so far.
+rotation_holder_2 = 0
 
-    for pin in range(0, 4):  # creates an index so goes to 0-3
-        xpin = StepPins[pin]
-        ypin = StepPins2[pin]
+while loop_control:  # sets up an infinite loop, to be broken when a condition is met later.
+    if rotation_holder_1 < num_rotations_1:  # if haven't completed the full sequence specified in args.
+        for pin in range(0, 4):  # turn each pin on or off for one of the sub lists.
+            xpin = step_pins[pin]
 
-        if Seq[StepCounter][pin] != 0:  # if it is 1 so turn on the pin
-            GPIO.output(xpin, True)
-            GPIO.output(ypin, True)
-        else:  # if it is 0 keep it off
-            GPIO.output(xpin, False)
-            GPIO.output(ypin, False)
+            if seq[step_counter_1][pin] != 0:
+                GPIO.output(xpin, True)
+            else:
+                GPIO.output(xpin, False)
+            step_counter_1 += step_dir  # increment stepCounter1 to move one along in list of lists.
 
-    StepCounter += StepDir  # moves along one in sequence
+        if step_counter_1 >= len(seq):  # If the four lists have been runtrough. reset counter. increment rotation holder to show one complete rotation of seq completed.
+            step_counter_1 = 0
+            rotation_holder_1 += 1
 
-    # If we reach the end of the sequence
-    # start again
-    if StepCounter >= StepCount:
-        StepCounter = 0
-    if StepCounter < 0:
-        StepCounter = StepCount + StepDir
+    if rotation_holder_2 < num_rotations_2:  # as above but for second motor
+        for pin in range(0, 4):
+            ypin = step_pins_2[pin]
 
-    # Wait before moving on
-    time.sleep(WaitTime)
+            if seq[step_counter_2][pin] != 0:
+                GPIO.output(ypin, True)
+            else:
+                GPIO.output(ypin, False)
+            step_counter_2 += step_dir
 
-GPIO.cleanup()
+        if step_counter_2 >= len(seq):
+            step_counter_2 = 0
+            rotation_holder_2 += 1
+
+    if (rotation_holder_1 >= num_rotations_1) and (rotation_holder_2 >= num_rotations_2):
+        # if both motors have completed their specified amount of rotations in args. Stop
+        loop_control = False
+    else:
+        time.sleep(wait_time)  # else wait and continue
