4f9051255f61b1a6a07815c2e8da5239c4b3485d
Jack Buckingham <jack_buckingham@blueyonder.co.uk>
2016-11-24 23:39:09 +0000

    #15 :: Replaced try-catch with messages for the assertions

--- hooks/commit-msg.py
+++ hooks/commit-msg.py
@@ -22,13 +22,9 @@ commit_msg_path = sys.argv[1]
 
 title_line_pattern = r'^#\d+ :: .*'
 
-try:
-    with open(commit_msg_path) as msg_file:
-        commit_message = msg_file.read().splitlines()
-        assert re.match(title_line_pattern, commit_message[0])
-        if len(commit_message) >= 2:
-            assert commit_message[1] == ''
-
-except AssertionError:
-    print("Invalid commit message format!")
-    raise
+with open(commit_msg_path) as msg_file:
+    commit_message = msg_file.read().splitlines()
+    assert len(commit_message) > 0, "Cannot commit with an empty commit message!"
+    assert re.match(title_line_pattern, commit_message[0]), "Title line does not match format"
+    if len(commit_message) >= 2:
+        assert commit_message[1] == '', "Title line must be followed by an empty line"
