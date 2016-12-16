b176044697c458390492ec88dba233642fd4f382
Jack Buckingham <jack_buckingham@blueyonder.co.uk>
2016-12-16 23:47:09 +0000

    #18 :: Added sophisticated argument parsing to move_linearly.py

--- Scripts/move_linearly.py
+++ Scripts/move_linearly.py
@@ -2,15 +2,24 @@
 
 import sys
 import time
+import argparse
 
 import Motors
 import StepperControl
 
 
 # Commandline arguments
-x_millimetres = float(sys.argv[1])
-y_millimetres = float(sys.argv[2])
-pen_millimetres_per_second = float(sys.argv[3])
+parser = argparse.ArgumentParser(description='Move the pen in a straight line using the low level linear move method.')
+parser.add_argument('x_millimetres', metavar='x', type=float,
+                    help='a %(type)s for the target x displacement in millimetres')
+parser.add_argument('y_millimetres', metavar='y', type=float,
+                    help='a %(type)s for the target y displacement in millimetres')
+parser.add_argument('-s', '--speed', metavar='SPEED', dest='pen_millimetres_per_second', type=float, default=32,
+                    help='the target speed for the pen in millimetres per second (default: %(default)smm/s)')
+parser.add_argument('-w', '--wait', type=float, default=0,
+                    help='an initial sleep time in seconds (default: %(default)s)')
+
+args = parser.parse_args()
 
 # Script body
 x_axis = StepperControl.Axis(motor=Motors.large_stepper_motor([22, 23, 24, 25]), lead=8)
@@ -19,10 +28,10 @@ both_motors = StepperControl.AxisPair(x_axis, y_axis)
 
 start_time = time.time()
 
-both_motors.move_linearly([x_millimetres, y_millimetres], pen_millimetres_per_second)
+both_motors.move_linearly([args.x_millimetres, args.y_millimetres], args.pen_millimetres_per_second)
 
 end_time = time.time()
 print("Elapsed: ", end='')
 print(end_time - start_time)
 print("Predicted: ", end='')
-print(((x_millimetres ** 2 + y_millimetres ** 2)**0.5) / pen_millimetres_per_second)
+print((args.x_millimetres ** 2 + args.y_millimetres ** 2) ** 0.5 / args.pen_millimetres_per_second)
