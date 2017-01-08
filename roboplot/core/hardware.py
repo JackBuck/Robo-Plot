121e088fef3ef7d6931390e0bb300ff0da05f8c9
Jack Buckingham <jack_buckingham@blueyonder.co.uk>
2017-01-08 22:41:23 +0000

    #43 :: Added empty hardware module

---
+++ roboplot/core/hardware.py
@@ -0,0 +1,8 @@
+"""
+A hardware repository.
+
+This module defines the specific instances of the hardware classes which should be used to communicate with our
+hardware.
+
+It is also where the GPIO pins used for each piece of hardware are defined.
+"""
