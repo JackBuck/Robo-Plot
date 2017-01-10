dd5fdcc1b5feba5cedac3a11e9ceab8aa0568183
Jack Buckingham <jack_buckingham@blueyonder.co.uk>
2017-01-10 23:18:51 +0000

    #44 :: Switch libraries depending on ROBOPLOT environment variable

--- roboplot/core/gpio/gpio_wrapper.py
+++ roboplot/core/gpio/gpio_wrapper.py
@@ -1,9 +1,17 @@
 """
 Wraps the GPIO library, so defining a single place to switch between the real GPIO library, and the emulator + gui.
+
+By default the emulator will be used. To use the RPi.GPIO module, export the environment variable ROBOPLOT=1 in the
+console before running (this should eventually be setup in the .bashrc of the pi).
+If for some reason you wish to do this through pycharm, it may be helpful to note that you can set extra environment
+variables available:
+ - to a script by editing its 'Run Configurations'
+ - in the console by going to 'File -> Settings -> Build, Execution, Deployment -> Console -> Python Console'
 """
 
+import os
 
-is_emulator = True  # Change this to False on the raspberry pi
+is_emulator = os.environ.get('ROBOPLOT', 0) == 0
 
 if is_emulator:
     from roboplot.core.gpio.EmulatorGUI import GPIO, app
