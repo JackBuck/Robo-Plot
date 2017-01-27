062373163e04e8d9124892fca5521eaf3665f466
HSenrab <hllewoh@gmail.com>
2017-01-27 12:46:12 +0000

    Iss #64 working camera sockets

--- roboplot/core/Camera/camera_client.py
+++ roboplot/core/Camera/camera_client.py
@@ -3,7 +3,7 @@ import socket
 import struct
 import sys
 import cv2
-import numpy
+import numpy as np
 
 
 # On robo_plot
@@ -35,34 +35,43 @@ print
 message = 'Take_Photo'
 
 try:
-    s.sendall(message)
+    s.sendall(bytes(message, 'UTF-8'))
 except socket.error:
     # Send failed
-    print
-    'Send Failed'
+    print('Send Failed')
     sys.exit()
 
-print
-'message sent succesfully'
+print('message sent successfully')
 
-# Now recieve data
+
+# Make a file-like object out of the connection
+connection = s.makefile('rwb')
+
+# Now receive data
 while True:
     # Read the length of the image as a 32-bit unsigned int. If the
     # length is zero, quit the loop
-    image_len = struct.unpack('<L', s.read(struct.calcsize('<L')))[0]
+
+    image_len = struct.unpack('<L', connection.read(struct.calcsize('<L')))[0]
     if not image_len:
         break
 
     # Construct a stream to hold the image data and read the image
     # data from the connection
     image_stream = io.BytesIO()
-    image_stream.write(s.read(image_len))
+    image_stream.write(connection.read(image_len))
 
-    # Rewind the stream, open it as an image with PIL and do some
+
+    # Rewind the stream, open it as an image with open cv and do some
     # processing on it
     image_stream.seek(0)
-    image = cv2.imread(image_stream)
-    print('Image is %dx%d' % image.shape)
-    cv2.imshow("Recieved Image", image)
+
+    # convert image into numpy array
+    data = np.fromstring(image_stream.getvalue(), dtype=np.uint8)
+    # turn the array into a cv2 image
+    image = cv2.imdecode(data, 1)
+    print('Image is %dx%d.%d' % image.shape)
+    cv2.imshow("Received Image", image)
+    cv2.imwrite("Image.jpeg", image)
 
 s.close()
