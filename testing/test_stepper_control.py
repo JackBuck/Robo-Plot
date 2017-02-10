c421c10103bded2451d7fd13fa443292728431a1
Jack Buckingham <jack_buckingham@blueyonder.co.uk>
2017-02-10 19:42:49 +0000

    #71 :: bugfix - needed to add steps_per_revolution attribute on mock motor

--- testing/test_stepper_control.py
+++ testing/test_stepper_control.py
@@ -2,23 +2,17 @@ import unittest
 from unittest.mock import MagicMock
 
 import roboplot.core.stepper_control as stepper_control
-import roboplot.core.limit_switches as limit_switches
-import roboplot.core.stepper_motors as stepper_motors
+from roboplot.core.limit_switches import LimitSwitch, UnexpectedLimitSwitchError
+from roboplot.core.stepper_motors import StepperMotor
 
 
 class AxisTest(unittest.TestCase):
 
     def setUp(self):
-        self._mock_limit_switches = (self._make_mock_switch(), self._make_mock_switch())
-        self._mock_motor = MagicMock(spec=stepper_motors.StepperMotor)
+        self._mock_limit_switches = (MagicMock(name='switch_1', spec_set=LimitSwitch, is_pressed=False),
+                                     MagicMock(name='switch_2', spec_set=LimitSwitch, is_pressed=False))
+        self._mock_motor = MagicMock(name='motor', spec_set=StepperMotor, steps_per_revolution=200)
         self._axis = stepper_control.Axis(self._mock_motor, 8, self._mock_limit_switches)
-
-    @staticmethod
-    def _make_mock_switch():
-        mock = MagicMock(spec=limit_switches.LimitSwitch)
-        assert hasattr(mock, 'is_pressed'), "Code has been refactored and test is out of date!"
-        mock.is_pressed = False
-        return mock
 
     def test_stepping_does_not_raise_when_no_limit_switch_is_pressed(self):
         self._axis.step()
@@ -28,7 +22,7 @@ class AxisTest(unittest.TestCase):
             self._reset_switches()
             self._mock_limit_switches[i].is_pressed = True
 
-            with self.assertRaises(limit_switches.UnexpectedLimitSwitchError):
+            with self.assertRaises(UnexpectedLimitSwitchError):
                 self._axis.step()
 
     def _reset_switches(self):
