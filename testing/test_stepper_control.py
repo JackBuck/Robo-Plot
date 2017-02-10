8fccf9600dfc47b9c7bf455c4f21144ceeac9a9f
Jack Buckingham <jack_buckingham@blueyonder.co.uk>
2017-02-10 22:41:33 +0000

    #71 :: Add test for a back-off move

--- testing/test_stepper_control.py
+++ testing/test_stepper_control.py
@@ -11,7 +11,7 @@ class AxisTest(unittest.TestCase):
     def setUp(self):
         self._mock_limit_switches = (MagicMock(name='switch_1', spec_set=LimitSwitch, is_pressed=False),
                                      MagicMock(name='switch_2', spec_set=LimitSwitch, is_pressed=False))
-        self._mock_motor = MagicMock(name='motor', spec_set=StepperMotor, steps_per_revolution=200)
+        self._mock_motor = MagicMock(name='motor', spec_set=StepperMotor, steps_per_revolution=200, clockwise=True)
         self._axis = stepper_control.Axis(self._mock_motor, 8, self._mock_limit_switches)
 
     def test_stepping_does_not_raise_when_no_limit_switch_is_pressed(self):
@@ -47,6 +47,27 @@ class AxisTest(unittest.TestCase):
         except UnexpectedLimitSwitchError:
             # We expect this error!
             pass
+
+    def test_current_location_shows_backoff_after_a_limit_switch_is_pressed(self):
+        self._axis.back_off_millimetres = 5
+
+        for i in range(10):
+            self._axis.step()
+
+        collision_location = self._axis.current_location
+        if self._axis.forwards:
+            expected_backoff_location = collision_location - 5
+        else:
+            expected_backoff_location = collision_location + 5
+        self._mock_limit_switches[0].is_pressed = True
+
+        try:
+            self._axis.step()
+        except UnexpectedLimitSwitchError:
+            pass
+
+        self.assertAlmostEqual(self._axis.current_location, expected_backoff_location,
+                               delta=self._axis.millimetres_per_step)
 
     def _reset_switches(self):
         for switch in self._mock_limit_switches:
