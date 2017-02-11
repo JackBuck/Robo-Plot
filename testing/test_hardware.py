3f489e5b7fcc76aa1896b7631dedae5782f242c1
Jack Buckingham <jack_buckingham@blueyonder.co.uk>
2017-02-11 00:02:02 +0000

    #71 :: Added test that hardware repository can be imported
    This checks that the syntax in that module is still correct.

---
+++ testing/test_hardware.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class HardwareRepositoryTest(unittest.TestCase):
+    @staticmethod
+    def test_can_import_hardware_module():
+        # noinspection PyUnresolvedReferences
+        import roboplot.core.hardware
+
+
+if __name__ == '__main__':
+    unittest.main()
