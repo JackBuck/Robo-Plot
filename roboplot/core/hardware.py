f3e38ecb154f60b482b297ff39d788da31f42197
Jack Buckingham <jack_buckingham@blueyonder.co.uk>
2017-02-11 00:10:02 +0000

    #71 :: Add limit switches to hardware class but raise in LimitSwitch.__init__

--- roboplot/core/hardware.py
+++ roboplot/core/hardware.py
@@ -7,6 +7,7 @@ hardware.
 It is also where the GPIO pins used for each piece of hardware are defined.
 """
 
+import roboplot.core.limit_switches as limit_switches
 import roboplot.core.stepper_motors as stepper_motors
 import roboplot.core.stepper_control as stepper_control
 
@@ -15,10 +16,14 @@ x_axis_motor = stepper_motors.large_stepper_motor(gpio_pins=(22, 23, 24, 25))
 y_axis_motor = stepper_motors.large_stepper_motor(gpio_pins=(19, 26, 20, 21))
 small_stepper_1 = stepper_motors.small_stepper_motor(gpio_pins=(5, 6, 12, 16))
 small_stepper_2 = stepper_motors.small_stepper_motor(gpio_pins=(2, 3, 4, 17))
+x_limit_switches = (limit_switches.LimitSwitch(),
+                    limit_switches.LimitSwitch())
+y_limit_switches = (limit_switches.LimitSwitch(),
+                    limit_switches.LimitSwitch())
 
 # Higher level objects
-x_axis = stepper_control.Axis(x_axis_motor, lead=8)
-y_axis = stepper_control.Axis(y_axis_motor, lead=8)
+x_axis = stepper_control.Axis(motor=x_axis_motor, lead=8, limit_switch_pair = x_limit_switches)
+y_axis = stepper_control.Axis(motor=y_axis_motor, lead=8, limit_switch_pair = y_limit_switches)
 
 if __debug__:
     both_axes = stepper_control.AxisPairWithDebugImage(y_axis, x_axis)
