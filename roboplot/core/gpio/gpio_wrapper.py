2eeabe8a2146221cc4ee48536d746987707058dd
Jack Buckingham <jack_buckingham@blueyonder.co.uk>
2017-02-13 22:13:30 +0000

    #82 :: Removed the call to quit the gui from gpio_wrapper.cleanup()

--- roboplot/core/gpio/gpio_wrapper.py
+++ roboplot/core/gpio/gpio_wrapper.py
@@ -24,7 +24,3 @@ GPIO.setmode(GPIO.BCM)
 # TODO: Look into using the atexit modue instead of calling directly in scripts? I'm not sure whether this will execute on exceptions though...
 def clean_up():
     GPIO.cleanup()
-
-    if is_emulator:
-        print('\nExiting Emulator GUI... (focus on another window to complete the exit)')
-        app.root.quit()  # Still needs tweaking -- something stops the gui from quitting when it has focus...
