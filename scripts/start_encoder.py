341af72ca15d4d74674d0435bf5852681f441c1f
Jack Buckingham <jack_buckingham@blueyonder.co.uk>
2017-02-15 23:55:08 +0000

    #45 :: Overhaul of start_encoder.py script
    Added argparse info, a choice of aces and infinite looping until ctrl+C.

--- scripts/start_encoder.py
+++ scripts/start_encoder.py
@@ -1,10 +1,33 @@
+#!/usr/bin/env python3
+
+import argparse
+import time
+
 import context
 import roboplot.core.hardware as hardware
 
-EncX = hardware.x_axis_encoder
+parser = argparse.ArgumentParser(description='Start an encoder.')
+parser.add_argument('encoder', type=str, choices=('x', 'y'), nargs='?', default='x',
+                    help='the encoder to run (default: %(default)s)')
+parser.add_argument('-r', '--refresh-frequency', type=float, default=24,
+                    help='the refresh rate / Hz (default: %(default)s)')
 
-for i in range(10000):
-    print("{0:06}".format(EncX.revolutions), end='\r')
+args = parser.parse_args()
 
-EncX.exit_thread()
-print("Program Ended")
+if args.encoder is "x":
+    encoder = hardware.x_axis_encoder
+elif args.encoder is "y":
+    encoder = hardware.y_axis_encoder
+else:
+    raise ValueError("Input encoder must be 'x', or 'y'.")  # The argument parser should stop us from getting here
+
+try:
+    print("Press ctrl-C to exit...")
+    while True:
+        print("\r{0: 7.1f} degrees".format(encoder.revolutions * 360), end='')
+        time.sleep(1/float(args.refresh_frequency))
+except KeyboardInterrupt:
+    pass
+finally:
+    encoder.exit_thread()
+    print("\nProgram Ended")
