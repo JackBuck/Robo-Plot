6513da1e6399d0bd11a71ac40bfbdbfebca95df0
Jack Buckingham <jack_buckingham@blueyonder.co.uk>
2017-02-17 14:44:37 +0000

    #81 :: Fix x-y limit switch problems

--- roboplot/core/hardware.py
+++ roboplot/core/hardware.py
@@ -16,10 +16,10 @@ x_axis_motor = stepper_motors.large_stepper_motor(gpio_pins=(22, 23, 24, 25))
 y_axis_motor = stepper_motors.large_stepper_motor(gpio_pins=(19, 26, 20, 21))
 small_stepper_1 = stepper_motors.small_stepper_motor(gpio_pins=(5, 6, 12, 16))
 small_stepper_2 = stepper_motors.small_stepper_motor(gpio_pins=(2, 3, 4, 17))
-x_limit_switches = (limit_switches.LimitSwitch(gpio_pin=9),  # Motor side
-                    limit_switches.LimitSwitch(gpio_pin=11)) # Encoder side
-y_limit_switches = (limit_switches.LimitSwitch(gpio_pin=8),  # Motor side
+x_limit_switches = (limit_switches.LimitSwitch(gpio_pin=8),  # Motor side
                     limit_switches.LimitSwitch(gpio_pin=7))  # Encoder side
+y_limit_switches = (limit_switches.LimitSwitch(gpio_pin=9),  # Motor side
+                    limit_switches.LimitSwitch(gpio_pin=11)) # Encoder side
 
 # Higher level objects
 x_axis = stepper_control.Axis(motor=x_axis_motor, lead=8, limit_switch_pair=x_limit_switches)
