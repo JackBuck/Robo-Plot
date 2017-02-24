22e563110a0c573e567c9dd58c384144ef6932b6
Jack Buckingham <jack_buckingham@blueyonder.co.uk>
2017-02-24 23:40:16 +0000

    #85 :: Added image preprocessing

--- scripts/recognise_number.py
+++ scripts/recognise_number.py
@@ -3,8 +3,10 @@
 import argparse
 import PIL.Image as Image
 
+import cv2
+import numpy as np
 import pytesseract
-
+import pytesseract.pytesseract
 
 # Commandline arguments
 parser = argparse.ArgumentParser(description='Recognise a supplied number.')
@@ -12,8 +14,17 @@ parser.add_argument('input_file', type=str, help='the path to the file containin
 args = parser.parse_args()
 
 # Script body
-recognised_text = pytesseract.image_to_string(
-    Image.open(args.input_file),
-    config='-psm 8 digits')  # psm 8 => single word; digits => use the digits config file supplied with the software
+img = cv2.imread(args.input_file, cv2.IMREAD_GRAYSCALE)
+if img is None:
+    raise TypeError("Could not open image file!")
+
+img = cv2.medianBlur(img, ksize=5)
+img = cv2.adaptiveThreshold(img, maxValue=255, adaptiveMethod=cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                            thresholdType=cv2.THRESH_BINARY, blockSize=11, C=2)
+img = Image.fromarray(img)
+
+# psm 8 => single word;
+# digits => use the digits config file supplied with the software
+recognised_text = pytesseract.image_to_string(img,config='-psm 8 digits')
 
 print(recognised_text)
