9f1fef553cda21311261f2337a7b593bd6583bf9
Jack Buckingham <jack_buckingham@blueyonder.co.uk>
2017-02-25 11:36:12 +0000

    #96 :: Rename variable for clarity

--- roboplot/imgproc/perimeter_search.py
+++ roboplot/imgproc/perimeter_search.py
@@ -13,34 +13,34 @@ def compute_positions(width, height, photo_size):
     """
 
     positions = []
-    current_position = (int(photo_size/2), int(photo_size/2))
+    current_path_point = (int(photo_size/2), int(photo_size/2))
     max_x_pos = width - int(photo_size/2)
     max_y_pos = height - int(photo_size/2)
 
     # Keep y at 0 and move along edge increasing in x.
-    while current_position[0] < max_y_pos:
-        positions.append(current_position)
-        current_position = (current_position[0] + photo_size, current_position[1])
+    while current_path_point[0] < max_y_pos:
+        positions.append(current_path_point)
+        current_path_point = (current_path_point[0] + photo_size, current_path_point[1])
 
-    current_position = (max_y_pos, current_position[1])
+    current_path_point = (max_y_pos, current_path_point[1])
 
     # Keep x max and move along edge increasing in y
-    while current_position[1] < max_x_pos:
-        positions.append(current_position)
-        current_position = (current_position[0], current_position[1] + photo_size)
+    while current_path_point[1] < max_x_pos:
+        positions.append(current_path_point)
+        current_path_point = (current_path_point[0], current_path_point[1] + photo_size)
 
-    current_position = (current_position[0], max_x_pos)
+    current_path_point = (current_path_point[0], max_x_pos)
 
     # Keep y max and move along edge decreasing in x
-    while current_position[0] > photo_size/2:
-        positions.append(current_position)
-        current_position = (current_position[0] - photo_size, current_position[1])
+    while current_path_point[0] > photo_size/2:
+        positions.append(current_path_point)
+        current_path_point = (current_path_point[0] - photo_size, current_path_point[1])
 
-    current_position = (int(photo_size/2), max_x_pos)
+    current_path_point = (int(photo_size/2), max_x_pos)
 
     # Keep x = 0 and move along edge decreasing in y
-    while current_position[1] > photo_size/2:
-        positions.append(current_position)
-        current_position = (current_position[0], current_position[1] - photo_size)
+    while current_path_point[1] > photo_size/2:
+        positions.append(current_path_point)
+        current_path_point = (current_path_point[0], current_path_point[1] - photo_size)
 
     return positions
