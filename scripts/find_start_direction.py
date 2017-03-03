717725fda651842b63ecf56bc6ba1f91640d1d02
hh139711 <Hannah.Howell@renishaw.com>
2017-03-03 09:38:45 +0000

    Iss#106: Added test for find start direction and moved it to a more appropriate place.

--- scripts/find_start_direction.py
+++ scripts/find_start_direction.py
@@ -2,7 +2,6 @@ import context
 import cv2
 import argparse
 import roboplot.imgproc.image_analysis as image_analysis
-import roboplot.imgproc.path_following as path_following
 import roboplot.core.config as config
 
 config.init_()
@@ -17,7 +16,7 @@ inFile = args.file_path
 
 image = cv2.imread(inFile, cv2.IMREAD_GRAYSCALE)
 
-start_direction = path_following.determine_starting_direction(image)
+start_direction = image_analysis.find_start_direction(image)
 
 print(start_direction)
 cv2.waitKey(0)
