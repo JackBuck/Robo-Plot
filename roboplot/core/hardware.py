ca746989111e5bcca0acb825a9d71a2fb5cda8d9
Jack Buckingham <jack_buckingham@blueyonder.co.uk>
2017-03-03 17:11:25 +0000

    #35 :: Change the home position

--- roboplot/core/hardware.py
+++ roboplot/core/hardware.py
@@ -33,8 +33,8 @@ x_limit_switches = (limit_switches.LimitSwitch(gpio_pin=8),  # Motor side
 y_limit_switches = (limit_switches.LimitSwitch(gpio_pin=9),  # Motor side
                     limit_switches.LimitSwitch(gpio_pin=11))  # Encoder side
 
-x_home_position = stepper_control.HomePosition(forwards=False, location=5)
-y_home_position = stepper_control.HomePosition(forwards=False, location=5)
+x_home_position = stepper_control.HomePosition(forwards=False, location=3)
+y_home_position = stepper_control.HomePosition(forwards=False, location=3)
 
 # Substitute objects
 if not config.real_hardware:
