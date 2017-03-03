6ad85d1b5f194b75ed87d04dcd330192aa642c81
hh139711 <Hannah.Howell@renishaw.com>
2017-03-03 12:06:21 +0000

    Iss#107: Initial attempt at full search.

--- roboplot/imgproc/perimeter_search.py
+++ roboplot/imgproc/perimeter_search.py
@@ -4,7 +4,7 @@ import cv2
 import roboplot.imgproc.image_analysis as IP
 
 
-def compute_positions(width, height, photo_size):
+def compute_perimeter_positions(width, height, photo_size):
     """
     Finds the positions needed for the camera to capture the entire border of the page.
     :param width:
@@ -45,3 +45,68 @@ def compute_positions(width, height, photo_size):
         current_path_point = (current_path_point[0], current_path_point[1] - photo_size)
 
     return positions
+    
+    
+def compute_positions(width, height, photo_size):
+    """
+    Finds the positions needed for the camera to capture the entire border of the page.
+    :param width:
+    :param height:
+    :param photo_size:
+    :return:
+    """
+
+    positions = []
+    current_path_point = (int(photo_size/2), int(photo_size/2))
+    max_x_pos = width - int(photo_size/2)
+    max_y_pos = height - int(photo_size/2)
+    
+    last_row = False
+
+    # Increment y.
+    while not last_row:
+    
+        if current_path_point[0] == max_y_pos:
+            last_row = True
+       
+        positions.append(current_path_point)
+        
+
+        #Increment x.
+        while current_path_point[1] < max_x_pos:
+            positions.append(current_path_point)
+            current_path_point = (current_path_point[0], current_path_point[1] + photo_size)
+            
+        current_path_point = (current_path_point[0], max_x_pos)
+        positions.append(current_path_point)
+        
+        # Increment y once.
+        
+        if last_row:
+            break
+        elif current_path_point[0] + photo_size > max_y_pos:
+            current_path_point = (max_y_pos, current_path_point[1])
+            last_row = True
+        else
+            current_path_point = (current_path_point[0] + photo_size, current_path_point[1])
+        
+        # Decrement x.
+        while current_path_point[1] > int(photo_size/2):
+            positions.append(current_path_point)
+            current_path_point = (current_path_point[0], current_path_point[1] - photo_size)
+            
+        current_path_point = (current_path_point[0], int(photo_size/2)
+        positions.append(current_path_point)
+        
+        current_path_point = (current_path_point[0] + photo_size, int(photo_size/2))
+        
+        
+        if last_row:
+            break   
+        elif current_path_point[0] > max_y_pos:
+            current_path_point[0] = max_y_pos
+   
+
+
+    return positions
+
