508f29d8dd16746e37ce8bf78af3814f3efa3d97
Hsenrab <Hannah.Howell@renishaw.com>
2017-03-07 07:55:37 +0000

    Iss#99, PullRequest #102, Made path changes and corrected to reference correct config file and removed redundant files.

--- roboplot/core/camera/remote_camera.py
+++ roboplot/core/camera/remote_camera.py
@@ -10,7 +10,7 @@ import time
 import cv2
 import numpy as np
 
-import roboplot.core.config as config
+import roboplot.config as config
 import roboplot.core.camera.camera_client as camera_client
 
 
@@ -25,7 +25,7 @@ class RemoteCamera:
             output = output[:200, :200, :]
 
             # Save photo.
-            cv2.imwrite(config.debug_output_folder + "Photo:" + str(self._photo_index) + ".jpg", output)
+            cv2.imwrite(os.path.join(config.debug_output_folder, "Photo:" + str(self._photo_index) + ".jpg"), output)
             self._photo_index += 1
 
             return output
