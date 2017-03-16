b633f07ab72a7764e257105d13a1a28d762c8766
HSenrab <hllewoh@gmail.com>
2017-03-16 18:25:34 +0000

    Iss#PI

--- scripts/calibrate.py
+++ scripts/calibrate.py
@@ -27,13 +27,21 @@ try:
         target_location = [hardware.both_axes.current_location[0] + cX * config.X_PIXELS_TO_MILLIMETRE_SCALE,
                            hardware.both_axes.current_location[1] + cY * config.Y_PIXELS_TO_MILLIMETRE_SCALE]
 
+        line_segment = curves.LineSegment(hardware.both_axes.current_location, target_location)
+        hardware.both_axes.follow(curve=line_segment, pen_speed=2)
+
+        cv2.imshow('Centre2', cv2.resize(photo, (500, 500)))
+        cv2.waitKey(0)
+
         hsv_image = cv2.cvtColor(photo, cv2.COLOR_BGR2HSV)
         (cX, cY) = cd.detect_green(hsv_image, 0, False)
 
         print(cY)
         print(cX)
 
-        line_segment = curves.LineSegment(hardware.both_axes.current_location, target_location)
+        l
+
+
 
     else:
         cv2.imshow('Centre', cv2.resize(photo, (500, 500)))
