347ff6f55024a758f76568fae53f37bef0b3f4a2
Jack Buckingham <jack_buckingham@blueyonder.co.uk>
2017-03-23 00:15:06 +0000

    #120 :: Added tests for bat images and fixed bug in creation of Number objects

--- testing/test_number_recognition.py
+++ testing/test_number_recognition.py
@@ -9,7 +9,7 @@ import unittest
 
 import context
 import roboplot.config as config
-from roboplot.dottodot.number_recognition import DotToDotImage
+import roboplot.dottodot.number_recognition as number_recognition
 
 
 class NumberRecognitionRegressionTests(unittest.TestCase):
@@ -17,26 +17,40 @@ class NumberRecognitionRegressionTests(unittest.TestCase):
 
     def test_on_selection_of_sizes(self):
         """Regression test number recognition on potentially rotated images."""
-        file_glob = os.path.join(self.test_data_directory, '*.jpg')
+        file_glob = os.path.join(self.test_data_directory, 'various_sizes', '*.jpg')
         for img_file in glob.glob(file_glob):
             filename = os.path.basename(img_file)
             file_name_match = re.match(
                 r'(?P<numeric_value>\d+)_(?P<fontsize>\d+)pt_(?P<angle>\d+)deg_y(?P<spot_y>\d+)_x(?P<spot_x>\d+)',
                 filename)
-            with self.subTest(filename = filename):
-                # Perform the number recognition
-                img = DotToDotImage.load_image_from_file(img_file)
-                number = img.process_image()
 
-                # Extract expected results
-                expected_number = int(file_name_match.group('numeric_value'))
-                expected_spot_location = (int(file_name_match.group('spot_y')),
-                                          int(file_name_match.group('spot_x')))
+            expected_number = number_recognition.Number(numeric_value=int(file_name_match.group('numeric_value')),
+                                                        dot_location_yx=(int(file_name_match.group('spot_y')),
+                                                                         int(file_name_match.group('spot_x'))))
+            self._test_on_file(img_file, expected_number)
 
-                # Compare
-                self.assertEqual(number.numeric_value, expected_number)
-                self.assertAlmostEqual(number.dot_location_yx[0], expected_spot_location[0], delta=2)
-                self.assertAlmostEqual(number.dot_location_yx[1], expected_spot_location[1], delta=2)
+    def test_on_bat_images(self):
+        """Regression test number recognition on images for the bat dot-to-dot"""
+        file_glob = os.path.join(self.test_data_directory, 'bat_size_20', '*.jpg')
+        for img_file in glob.glob(file_glob):
+            filename = os.path.basename(img_file)
+            file_name_match = re.match(r'(?P<numeric_value>\d+)_y(?P<spot_y>\d+)_x(?P<spot_x>\d+)', filename)
+
+            expected_number = number_recognition.Number(numeric_value=int(file_name_match.group('numeric_value')),
+                                                        dot_location_yx=(int(file_name_match.group('spot_y')),
+                                                                         int(file_name_match.group('spot_x'))))
+            self._test_on_file(img_file, expected_number)
+
+    def _test_on_file(self, file_path, expected_number):
+        with self.subTest(filename=os.path.basename(file_path)):
+            # Perform the number recognition
+            img = number_recognition.DotToDotImage.load_image_from_file(file_path)
+            number = img.process_image()
+
+            # Compare
+            self.assertEqual(number.numeric_value, expected_number.numeric_value)
+            self.assertAlmostEqual(number.dot_location_yx[0], expected_number.dot_location_yx[0], delta=2)
+            self.assertAlmostEqual(number.dot_location_yx[1], expected_number.dot_location_yx[1], delta=2)
 
 
 if __name__ == '__main__':
