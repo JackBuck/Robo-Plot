3db59aa1cd70cfb90e1b715df9de7732bff6a192
Jack Buckingham <jack_buckingham@blueyonder.co.uk>
2017-03-24 16:27:20 +0000

    #122 :: Renamed distance_between_photos to millimetres_between_photos

--- roboplot/imgproc/page_search.py
+++ roboplot/imgproc/page_search.py
@@ -44,22 +44,22 @@ def compute_perimeter_positions(width, height, photo_size):
     return positions
 
 
-def compute_positions(width: int, height: int, photo_size: int, distance_between_photos: int = None):
+def compute_positions(width: int, height: int, photo_size: int, millimetres_between_photos: int = None):
     """Finds the positions needed for the camera to capture the entire page.
     
     Args:
         width (int): The width of the paper (x axis).
         height (int): The height of the paper (y axis).
         photo_size (int): The size of the square photo in mm.
-        distance_between_photos (int): The distance in mm used to offset consecutive photos. If left as None, then the
-                                       photo_size will be used instead.
+        millimetres_between_photos (int): The distance in mm used to offset consecutive photos. If left as None, then
+                                          the photo_size will be used instead.
 
     Returns:
         positions (list of points) The path required to scan entire page.
     """
 
-    if distance_between_photos is None:
-        distance_between_photos = photo_size
+    if millimetres_between_photos is None:
+        millimetres_between_photos = photo_size
 
     positions = []
     current_path_point = [int(photo_size / 2), int(photo_size / 2)]
@@ -79,7 +79,7 @@ def compute_positions(width: int, height: int, photo_size: int, distance_between
         # Increment x.
         while current_path_point[1] < max_x_pos:
             positions.append(current_path_point.copy())
-            current_path_point[1] += distance_between_photos
+            current_path_point[1] += millimetres_between_photos
 
         current_path_point[1] = max_x_pos
         positions.append(current_path_point.copy())
@@ -87,21 +87,21 @@ def compute_positions(width: int, height: int, photo_size: int, distance_between
         # Increment y once.   
         if last_row:
             break
-        elif current_path_point[0] + distance_between_photos > max_y_pos:
+        elif current_path_point[0] + millimetres_between_photos > max_y_pos:
             current_path_point[0] = max_y_pos
             last_row = True
         else:
-            current_path_point[0] += distance_between_photos
+            current_path_point[0] += millimetres_between_photos
 
         # Decrement x.
         while current_path_point[1] > int(photo_size / 2):
             positions.append(current_path_point.copy())
-            current_path_point[1] -= distance_between_photos
+            current_path_point[1] -= millimetres_between_photos
 
         current_path_point[1] = int(photo_size / 2)
         positions.append(current_path_point.copy())
 
-        current_path_point[0] += distance_between_photos
+        current_path_point[0] += millimetres_between_photos
 
         if current_path_point[0] > max_y_pos:
             current_path_point[0] = max_y_pos
