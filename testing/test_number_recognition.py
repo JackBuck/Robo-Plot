8dbc197889f657dacb7f8773bb2f91395c65bee7
Jack Buckingham <jack_buckingham@blueyonder.co.uk>
2017-03-28 19:50:24 +0100

    #122 :: Bugfix - got wrong name for dot_location_yx_pixels

--- testing/test_number_recognition.py
+++ testing/test_number_recognition.py
@@ -56,7 +56,7 @@ class NumberRecognitionRegressionTests(unittest.TestCase):
             for expected_number in expected_numbers:
                 recognised_numbers_at_this_location =\
                     [n for n in img.recognised_numbers
-                     if np.allclose(n.dot_location_yx, expected_number.dot_location_yx, rtol=0, atol=2)]
+                     if np.allclose(n.dot_location_yx_pixels, expected_number.dot_location_yx, rtol=0, atol=2)]
 
                 self.assertEqual(len(recognised_numbers_at_this_location), 1,
                                  'Did not find a unique matching recognised number at (y{0[0]}x{0[1]}).'.format(
