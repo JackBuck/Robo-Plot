9087427cab0cb9f5fcaf068b5ee1736fb36cb913
Jack Buckingham <jack_buckingham@blueyonder.co.uk>
2017-03-29 20:25:09 +0100

    #122 :: Update the dot_to_dot.py script with the new dummy camera

--- scripts/dot_to_dot.py
+++ scripts/dot_to_dot.py
@@ -17,6 +17,7 @@ import roboplot.imgproc.page_search as page_search
 import roboplot.dottodot.clustering as clustering
 import roboplot.dottodot.number_recognition as number_recognition
 import roboplot.svg.svg_parsing as svg_parsing
+from roboplot.core.camera.dummy_camera_from_image_paths import DummyCameraFromImagePaths
 from roboplot.core.gpio.gpio_wrapper import GPIO
 
 
@@ -30,14 +31,19 @@ try:
     # Hannah has put a take_photo() method on the plotter, so we just need to also funnel the page_search method into the
     # plotter as well somehow.
     plotter = hardware.plotter
-    camera = hardware.camera
+
+    # camera = hardware.camera
+    image_paths = sorted(glob.glob('/home/jack/Documents/SoftwareDevelopment/Projects/Hackspace2016-2017/Resources'
+                                   '/OutputFromDotToDot_Bat_3/Photo*.jpg'))
+    camera = DummyCameraFromImagePaths(resolution_pixels_xy=(200, 200), pixels_to_mm_scale_factors_xy=(0.233, 0.237),
+                                       image_paths=image_paths)
 
     # Scan the bed for photos
     a4_height_y_mm = 297
     a4_width_x_mm = 210
     target_positions = page_search.compute_positions(a4_width_x_mm, a4_height_y_mm,
-                                                     photo_size=int(camera.resolution_mm[0]),
-                                                     millimetres_between_photos=int(camera.resolution_mm[0]/2))
+                                                     photo_size=int(camera.resolution_mm_xy[0]),
+                                                     millimetres_between_photos=int(camera.resolution_mm_xy[0] / 2))
 
     plotter.home()
 
@@ -45,17 +51,10 @@ try:
     start_time = time.time()
 
     recognised_numbers = []
-    image_paths = sorted(glob.glob('/home/jack/Documents/SoftwareDevelopment/Projects/Hackspace2016-2017/Resources'
-                                   '/OutputFromDotToDot_Bat_2/Photo*.jpg'))
-    # i=0
     for target_position in target_positions:
         plotter.move_camera_to(target_position)
         photo = camera.take_photo_at(target_position)
         photo = cv2.cvtColor(photo, cv2.COLOR_BGR2GRAY)
-        # print(target_position)
-        # print(image_paths[i])
-        # photo = cv2.imread(image_paths[i], cv2.IMREAD_GRAYSCALE)
-        # i += 1
 
         dot_to_dot_image = number_recognition.DotToDotImage(photo)
         dot_to_dot_image.process_image()
@@ -68,11 +67,13 @@ try:
         recognised_numbers.extend(new_global_numbers)
 
     end_time = time.time()
-    print('Time to collect photos: {:.1f} seconds'.format(end_time-start_time))
+    print('Time to collect photos: {:.1f} seconds'.format(end_time - start_time))
+
 
     # Filter the results
     def millimetres_between_numbers(first: number_recognition.GlobalNumber, second: number_recognition.GlobalNumber):
         return np.linalg.norm(first.dot_location_yx_mm - second.dot_location_yx_mm)
+
 
     groups = clustering.group_objects(recognised_numbers,
                                       distance_function=millimetres_between_numbers,
@@ -105,7 +106,8 @@ try:
             dot_to_dot_image.process_image()
             dot_to_dot_image.print_recognised_local_numbers()
 
-            global_numbers = [number_recognition.GlobalNumber(n, location_yx_mm) for n in dot_to_dot_image.recognised_numbers]
+            global_numbers = [number_recognition.GlobalNumber.from_local(n, location_yx_mm) for n in
+                              dot_to_dot_image.recognised_numbers]
             group.extend([n for n in global_numbers if np.linalg.norm(n.dot_location_yx_mm - location_yx_mm) < 5])
 
             # Try again to get the mode values
@@ -127,7 +129,7 @@ try:
             break
 
     # Draw the dot-to-dot
-    path_curve = [curves.LineSegment(final_numbers[i-1].dot_location_yx_mm, final_numbers[i].dot_location_yx_mm)
+    path_curve = [curves.LineSegment(final_numbers[i - 1].dot_location_yx_mm, final_numbers[i].dot_location_yx_mm)
                   for i in range(1, len(final_numbers))]
     if len(final_numbers) > 0:
         path_curve.append(curves.LineSegment(final_numbers[-1].dot_location_yx_mm, final_numbers[0].dot_location_yx_mm))
