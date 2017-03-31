5107b5d388a6f0a145675e1b47e3ce1a7213ced6
Jack Buckingham <jack_buckingham@blueyonder.co.uk>
2017-03-31 17:11:31 +0100

    #hotfix :: Copy the debug output to a subfolder of debug images
    This is because I keep accidentally deleting it by setting the pen position

--- scripts/dot_to_dot.py
+++ scripts/dot_to_dot.py
@@ -3,15 +3,26 @@
 import argparse
 import time
 import glob
-import pickle
+import os
+import shutil
 
 import context
+import roboplot.config as config
 import roboplot.core.hardware as hardware
-import roboplot.dottodot.curve_creation as curve_creation
 from roboplot.dottodot.dot_to_dot_plotter import DotToDotPlotter
 from roboplot.core.camera.dummy_camera_from_image_paths import DummyCameraFromImagePaths
 from roboplot.core.gpio.gpio_wrapper import GPIO
 
+
+def find_free_debug_images_subfolder() -> str:
+    desired_name = 'DotToDot_$1'
+    new_folder = None
+    i = 0
+    while new_folder is None or os.path.exists(new_folder):
+        new_folder = os.path.join(config.debug_output_folder, desired_name.replace('$1', str(i)))
+        i += 1
+
+    return new_folder
 
 try:
     # Commandline arguments
@@ -29,12 +40,16 @@ try:
                                                                       image_paths=image_paths)
     plotter = DotToDotPlotter(hardware.plotter)
 
-
     # Do the dot-to-dot
     start_time = time.time()
     plotter.do_dot_to_dot()
     end_time = time.time()
     print('Elapsed: {:.0f} seconds'.format(end_time - start_time))
+
+    debug_images_copy_target = find_free_debug_images_subfolder()
+    os.mkdir(debug_images_copy_target)
+    for f in glob.glob(os.path.join(config.debug_output_folder, '*.jpg')):
+        shutil.copy2(f, debug_images_copy_target)
 
 finally:
     GPIO.cleanup()
